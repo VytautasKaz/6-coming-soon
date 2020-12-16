72ed58e7ff70af722ce0050613aaa356bf612296
VytautasKaz <VytautasKaz@users.noreply.github.com>
2020-12-16 12:29:31 +0200

    progress bar validation

--- js/components/progress-bar/renderProgressBar.js
+++ js/components/progress-bar/renderProgressBar.js
@@ -1,7 +1,19 @@
+import {isValidInput} from './isValidInput.js';
+import {isValidProgressBar} from './isValidProgressBar.js';
 function renderProgressBar(selector, data) {
+    if (!isValidInput(selector, data)) {
+        return false;
+    }
     let HTML = '';
+    let DOM = document.querySelector(selector);
+    if (!DOM) {
+        return false;
+    }
     for (let i = 0; i < data.length; i++) {
         const bar = data[i];
+        if (!isValidProgressBar(bar)){
+            continue;
+        }
         HTML += `<div class="progress-bar">
                     <div class="top">
                         <div class="label">${bar.title}</div>
@@ -14,7 +26,10 @@ function renderProgressBar(selector, data) {
                     </div>
                 </div>`;
     }
-    let DOM = document.querySelector(selector);
+    if (HTML === ''){
+        return false;
+    }
     DOM.innerHTML += HTML;
+    return true;
 }
 export {renderProgressBar}
